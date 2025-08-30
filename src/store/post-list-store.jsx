013281c1c21c9709b6006e7fd70f2a4655e69569
subhendusahu069 <subhendusahu069@gmail.com>
2025-08-30 23:45:59 +0530

    add files

---
+++ src/store/post-list-store.jsx
@@ -0,0 +1,30 @@
+import { createContext, useReducer } from "react";
+// import Postlist from "../components/Postlist";
+
+const PostList= createContext({
+  postList:[],
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const postListReducer = (currPostList, action) =>{
+  return currPostList;
+}
+const PostListProvider = (children) => {
+  const [postList,dispatchPostList] = useReducer(postListReducer,[]);
+
+  const addPost = () =>{
+
+  }
+
+  const deletePost = () =>
+  {
+
+  }
+return(
+<PostList.Provider value={{ postList, addPost, deletePost }}>
+{children}
+</PostList.Provider>
+);
+};
+export default PostListProvider;
