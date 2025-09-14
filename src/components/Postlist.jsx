8a95bde861ccb750d5aa752c27f5901999a0379e
subhendusahu069 <subhendusahu069@gmail.com>
2025-09-14 13:20:47 +0530

    Use of useEfect

--- src/components/Postlist.jsx
+++ src/components/Postlist.jsx
@@ -1,28 +1,56 @@
 import Post from "./Post";
-import { useContext } from "react";
+// import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { PostList as postListData } from "../store/postliststore";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 const Postlist = () => {
   const { postList, addInitalPost } = useContext(postListData);
+  // const [dataFatch, setDataFatch] = useState(false);
+  const [fatch, setFatch] = useState(false);
   const postArray = Array.isArray(postList)
     ? postList
     : Object.values(postList);
-  const handleGetPostClick = () => {
-    fetch("https://dummyjson.com/posts")
+
+  const controller = new AbortController();
+  const signal = controller.signal;
+  
+  useEffect(() => {
+    setFatch(true);
+    fetch("https://dummyjson.com/posts",{ signal })
       .then((res) => res.json())
       .then((data) => {
         addInitalPost(data.posts);
+        setFatch(false);
       });
-  };
+    return () => controller.abort();
+    
+  }, []);
+
+  // if (!dataFatch) {
+  //   fetch("https://dummyjson.com/posts")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       addInitalPost(data.posts);
+  //     });
+  //   setDataFatch(true);
+  // }
+
+  // const handleGetPostClick = () => {
+  //   fetch("https://dummyjson.com/posts")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       addInitalPost(data.posts);
+  //     });
+  // };
 
   return (
     <>
-      {postList.length === 0 && (
-        <WelcomeMessage onGetPostClick={handleGetPostClick} />
-      )}
+      {fatch && <LoadingSpinner />}
+      {!fatch && postList.length === 0 && <WelcomeMessage />}
 
-      {postArray.map((post) => (
+      {!fatch && postArray.map((post) => (
         <Post key={post.id} post={post} />
       ))}
     </>
